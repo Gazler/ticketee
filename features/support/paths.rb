6f5a4bbe5c78559414afa487246906c889b935df
Gary Rennie <webmaster@gazler.com>
2011-02-09 15:00:23 +0000

    Implemented tickets display in projects

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -12,7 +12,11 @@ module NavigationHelpers
       '/'
 
     when /the project page for "([^\"]*)"/
-      project_path(Project.find_by_name($1))
+      project_path(project($1))
+
+    when /the "([^\"]*)" ticket in the "([^\"]*)" project/
+      project_ticket_path(project($2), Ticket.find_by_title!($1))
+
 
 
     # Add more mappings here.
@@ -32,6 +36,12 @@ module NavigationHelpers
       end
     end
   end
+
+  def project(name)
+   Project.find_by_name!(name)
+  end
+
+
 end
 
 World(NavigationHelpers)
