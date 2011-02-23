0826aa960d92273f0be0ea11eda6690bf667a9fc
Gary Rennie <webmaster@gazler.com>
2011-02-23 10:49:04 +0000

    Authentication when creating tickets

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -4,3 +4,9 @@ Given /^that project has a ticket:$/ do |table|
   end
 end
 
+Given /^"([^\"]*)" has created a ticket for this project:$/ do |email, table|
+  table.hashes.each do |attributes|
+    @project.tickets.create!(attributes.merge!(:user => User.find_by_email!(email)))
+  end
+end
+
