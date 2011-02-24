f73dfec13bc16f5322cea492bccff5b2d0aee975
Gary Rennie <webmaster@gazler.com>
2011-02-24 11:20:09 +0000

    Hidden links for non-admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,13 @@ module ApplicationHelper
     @title = (parts << "Ticketee").join(" - ") unless parts.empty?
     @title || "Ticketee"
   end
+
+  def admins_only(&block)
+    if current_user && current_user.admin?
+      block.call
+    end
+    nil
+  end
+
 end
 
