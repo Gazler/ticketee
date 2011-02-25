96701092fa9bc2c29bbc98c9d6181334cd79a6ab
Gary Rennie <webmaster@gazler.com>
2011-02-25 15:46:45 +0000

    ticket restrictions scoped to project

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe TicketsController do
+  let(:user) { create_user! }
+  let(:project) { Factory(:project) }
+  let(:ticket) { Factory(:ticket, :project => project) }
+
+  context "standard users" do
+    it "cannot acces a ticket for a project they don't have access to" do
+      sign_in(:user, user)
+      get :show, :id => ticket.id, :project_id => project.id
+      response.should redirect_to(root_path)
+      flash[:alert].should eql("The project you were looking for could not be found.")
+    end
+  end
 
 end
+
