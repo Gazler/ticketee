8a8bc9287857b3b37732ad5baf46ee4b70f81006
Gary Rennie <webmaster@gazler.com>
2011-03-04 09:50:31 +0000

    Added API to create projects

--- app/controllers/api/v1/project_controller.rb
+++ app/controllers/api/v1/project_controller.rb
@@ -2,5 +2,15 @@ class Api::V1::ProjectsController < Api::V1::BaseController
   def index
     respond_with(Project.all)
   end
+
+  def create
+  project = Project.create(params[:project])
+    if project.valid?
+      respond_with(project, :location => api_v1_project_path(project))
+    else
+      respond_with(project)
+    end
+  end
+
 end
 
--- spec/api/v1/project_spec.rb
+++ spec/api/v1/project_spec.rb
@@ -21,7 +21,7 @@ describe "/api/v1/project", :type => :api do
     context "index" do
       let(:url) { "/api/v1/projects" }
       it "JSON" do
-        get "#{url}.json", "token="+token
+        get "#{url}.json", :token => token
         last_response.status.should eql(200)
         last_response.body.should eql(Project.readable_by(user).to_json)
         projects = JSON.parse(last_response.body)
@@ -29,12 +29,32 @@ describe "/api/v1/project", :type => :api do
       end
 
       it "XML" do
-        get "#{url}.xml", "token="+token
+        get "#{url}.xml", :token => token
         last_response.body.should eql(Project.readable_by(user).to_xml)
         projects = Nokogiri::XML(last_response.body)
         projects.css("project name").text.should eql("Inspector")
       end
+    end
+  end
 
+  context "creating a project" do
+    let(:url) { "/api/v1/projects" }
+
+    it "sucessful JSON" do
+      post "#{url}.json", :token => token,
+                          :project => {
+                            :name => "Ticketee"
+                          }
+      last_response.status.should eql(201)
+      last_response.body.should eql(Project.find_by_name("Ticketee").to_json)
+    end
+
+   it "unsuccessful JSON" do
+      post "#{url}.json", :token => token,
+                          :project => {}
+      last_response.status.should eql(422)
+      errors = {"name" => "can't be blank"}
+      last_response.body.should eql(errors.to_json)
     end
   end
 end
